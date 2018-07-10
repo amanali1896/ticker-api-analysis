49407d72eaf2f0ef37679f3ae8c2efec47a239da
Aman Ali <pamanalionline@gmail.com>
2018-07-10 00:07:57 -0400

    Add files via upload

---
+++ ticker-api-analysis.py
@@ -0,0 +1,6 @@
+#install "requests" using pip install requests
+#install "JSON" using pip install json
+#this api displays cryptocurrency ticker data in order of rank. The maximum number of results per call is 100.
+#Pagination is possible by using the start and limit parameters.
+import json #importing the libraries
+import requests #importing the libraries
